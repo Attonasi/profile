d3c91a5c6f5ec786db258baf7712c47d88a59869
Attonasi <attonasi@hotmail.com>
2017-01-05 17:52:39 -0800

    I am leaving too late to make a new branch

---
+++ js/userStories.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var userStories = [
+  {
+    title:       'User Story Day 1',
+    author:      'Da Man',
+    mvpStory:    '1',
+    // authorUrl:   'http://www.imdb.com/name/nm0000102/',
+    publishedOn: '2017-1-5',
+    body: `As a developer, I need my site to use valid and semantic markup, so that employers will love me.
+    As the creator, I need the page to link to my social and GitHub pages, so that visitors can follow me and I can build my audience.
+    As a developer, I want wireframes for each of the main views in my portfolio, so I will have a concrete idea of the design I am building.
+    As a developer, I want to make a data model (object constructor) for my portfolio data, so I can store individual projects and render them to the DOM.
+    As a visitor, I want the images to be responsive, so that content stays properly proportioned.
+    As a visitor, I want the viewport properly sized, so that content fits all the size I have available.
+    As a visitor, I want the primary nav to be responsive, so that I can get around using any device.`
+  },
+  {
+    title:       'User Story Day 2',
+    author:      'Da Man',
+    mvpStory:    '2',
+    // authorUrl:   'http://www.imdb.com/name/nm0000102/',
+    publishedOn: '2017-1-6',
+    body:`As a developer, I want my site to use valid and semantic markup, so that employers will love me.
+As the creator, I want the page to link to my social and GitHub pages, so that visitors can follow me, and I can build my audience.
+As a developer, I want portfolio items displayed with a repeatable template, so that I can reuse it, and abstract out the details for individual projects.
+As a developer, I want my CSS organized (think SMACSS) so that I can find my stylings swiftly.`
+  },
+  {
+    title:       'User Story Day 3',
+    author:      'Da Man',
+    mvpStory:    '3',
+    // authorUrl:   'http://www.imdb.com/name/nm0000102/',
+    publishedOn: '2017-1-7',
+    body:`As the creator, I want the Home and About nav links to act as tabs, so my story is revealed FAST. This means your links to NOT navigate to a new page. Instead, your "single page app" shows only the section related to the navigation tab that is selected.You can use 'data-' attributes to associate a content section with a particular tab. Then use jQuery so when the tab is clicked, you hide all the sections, then reveal the associated section only.
+As a reader, I want the portfolio to use a nice color scheme, so that it stands out visually.`
+  },
+  {
+    title:       'User Story Day 4',
+    author:      'Da Man',
+    mvpStory:    '4',
+    // authorUrl:   'http://www.imdb.com/name/nm0000102/',
+    publishedOn: '2017-1-7',
+    body:`As a developer, I want to use Handlebars for my project template, so that I can include new projects more easily.
+As a developer, I want my CSS styles to follow SMACSS organization, so that I know where to look for creating and editing styles.`
+  }
+];
