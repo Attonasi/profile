e3814295ca86e06f6a0e697df49b2899ba91376a
Attonasi <attonasi@hotmail.com>
2017-01-08 07:52:43 -0800

    arg

--- js/userStories.js
+++ js/userStories.js
@@ -5,7 +5,6 @@ var userStories = [
     title:       'User Story Day 1',
     author:      'Da Man',
     mvpStory:    '1',
-    // authorUrl:   'http://www.imdb.com/name/nm0000102/',
     publishedOn: '2017-1-5',
     body: 'As a developer, I need my site to use valid and semantic markup, so that employers will love me.<br>As the creator, I need the page to link to my social and GitHub pages, so that visitors can follow me and I can build my audience.<br>As a developer, I want wireframes for each of the main views in my portfolio, so I will have a concrete idea of the design I am building.<br>As a developer, I want to make a data model (object constructor) for my portfolio data, so I can store individual projects and render them to the DOM.<br>As a visitor, I want the images to be responsive, so that content stays properly proportioned.<br>As a visitor, I want the viewport properly sized, so that content fits all the size I have available.<br>As a visitor, I want the primary nav to be responsive, so that I can get around using any device.'
   },
@@ -13,7 +12,6 @@ var userStories = [
     title:       'User Story Day 2',
     author:      'Da Man',
     mvpStory:    '2',
-    // authorUrl:   'http://www.imdb.com/name/nm0000102/',
     publishedOn: '2017-1-6',
     body:'As a developer, I want my site to use valid and semantic markup, so that employers will love me.<br>As the creator, I want the page to link to my social and GitHub pages, so that visitors can follow me, and I can build my audience.<br>As a developer, I want portfolio items displayed with a repeatable template, so that I can reuse it, and abstract out the details for individual projects.<br>As a developer, I want my CSS organized (think SMACSS) so that I can find my stylings swiftly.'
   },
@@ -21,7 +19,6 @@ var userStories = [
     title:       'User Story Day 3',
     author:      'Da Man',
     mvpStory:    '3',
-    // authorUrl:   'http://www.imdb.com/name/nm0000102/',
     publishedOn: '2017-1-7',
     body:'As the creator, I want the Home and About nav links to act as tabs, so my story is revealed FAST. This means your links to NOT navigate to a new page. Instead, your "single page app" shows only the section related to the navigation tab that is selected.You can use data- attributes to associate a content section with a particular tab. Then use jQuery so when the tab is clicked, you hide all the sections, then reveal the associated section only.<br>As a reader, I want the portfolio to use a nice color scheme, so that it stands out visually.'
   },
@@ -29,8 +26,15 @@ var userStories = [
     title:       'User Story Day 4',
     author:      'Da Man',
     mvpStory:    '4',
-    // authorUrl:   'http://www.imdb.com/name/nm0000102/',
     publishedOn: '2017-1-7',
     body:'As a developer, I want to use Handlebars for my project template, so that I can include new projects more easily.<br>As a developer, I want my CSS styles to follow SMACSS organization, so that I know where to look for creating and editing styles.'
+  },
+  {
+    title:       'User Story Day 5',
+    author:      'Da Man',
+    mvpStory:    '4',
+    publishedOn: '2017-1-7',
+
+    body: `We have two new libraries that we'll be working with today; highlight.js and marked.js. Focusing on the functionality of adding a new article through a form submission, complete the TODOs in articleView.js. Reminder: use template literals rather than string concatenation where applicable`
   }
 ];
---
+++ js/widthvarset.js
@@ -0,0 +1,10 @@
+
+
+
+var screenWidth = ($(screen).width() * .9);
+var screenHeight = ($(window).height() * .9);
+
+('#smurf').height(screenHeight);
+('#smurf').attr('width').val(screenWidth);
+
+console.log(screenWidth, screenHeight);
